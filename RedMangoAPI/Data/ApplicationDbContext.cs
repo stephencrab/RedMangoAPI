0f13b00c20eeb60b9a8c6ff37add4986e2a3a8e5
ASVT_Stephen <stephen_hu@asiavista.com.tw>
2023-03-24 21:48:22 +0800

    Basic Setup

---
+++ RedMangoAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RedMangoAPI.Models;
+
+namespace RedMangoAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+    }
+}
---
+++ RedMangoAPI/Models/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedMangoAPI.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SpecialTag { get; set; }
+        public string Category { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Price { get; set; }
+        [Required]
+        public string Image { get; set; }
+    }
+}
